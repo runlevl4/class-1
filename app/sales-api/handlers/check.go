3e5b1363eb5addfabb0fe28c38b96027f374a616
William Kennedy <bill@ardanlabs.com>
2020-09-24 12:34:44 -0400

    lesson 6

--- app/sales-api/handlers/check.go
+++ app/sales-api/handlers/check.go
@@ -16,14 +16,12 @@ type check struct {
 
 func (c check) readiness(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
 	if n := rand.Intn(100); n%2 == 0 {
-		return errors.New("untrusted error")
+		return web.NewRequestError(errors.New("trusted error"), http.StatusBadRequest)
 	}
-
 	status := struct {
 		Status string
 	}{
 		Status: "OK",
 	}
-	log.Println(r, status)
 	return web.Respond(ctx, w, status, http.StatusOK)
 }
